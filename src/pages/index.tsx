4a74dbef2e0a7b822a50c842fd43284aef16c0d8
Matheus Sixel <msixel22@gmail.com>
2025-08-13 14:56:14 -0300

    Criando layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
-import { Header } from "@/components/header";
 
 export default function Home() {
   return (
     <>
       <div className="min-h-screen min-w-full bg-slate-500">
-        <Header/>
       </div>
     </>
   );
