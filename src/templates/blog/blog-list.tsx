eff2e3c7d7358ef2c56316d568acfdb8e4a2500a
Matheus Sixel <msixel22@gmail.com>
2025-09-22 16:37:07 -0300

    Criando funcionalidade buscar

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -2,14 +2,16 @@ import { Search } from "@/components/search";
 import { useRouter } from "next/router";
 import { PostCard } from "./components/post-card";
 import { PostGridCard } from "./components/post-grid-card";
-import { allPosts } from "contentlayer/generated";
+import { allPosts, type Post } from "contentlayer/generated";
+import { Inbox } from "lucide-react";
 
 export function BlogList() {
   const router = useRouter();
   const query = router.query.q as string;
   const pageTitle = query ? `Resultados de busca para "${query}"` : 'Dicas e estratégias para impulsionar o seu negócio';
 
-  const posts = allPosts
+  const posts = query ? allPosts.filter((post) => post.title.toLowerCase().includes(query.toLowerCase())) : allPosts;
+  const hasPosts = posts.length > 0;
 
   return(
     <div className="flex flex-col py-24 flex-grow h-full">
@@ -30,11 +32,11 @@ export function BlogList() {
       </header>
 
       {/*Listagem de posts */}
-      <PostGridCard>
+      {hasPosts && (
+        <PostGridCard>
         {posts.map((post) =>(
         <PostCard
         key={post._id}
-
           title={post.title}
           description={post.description}
           date={new Date (post.date).toLocaleDateString('pt-BR')}
@@ -43,10 +45,20 @@ export function BlogList() {
           author={{
             avatar: post.author.avatar,
             name: post.author.name,
-      }}
+            }}
         />
         ))}
       </PostGridCard>
+    )}
+
+    {!hasPosts && (
+      <div className="container px-8">
+        <div className="flex flex-col items-center justify-center gap-8 border-dashed border-2 border-gray-300 p-8 md:p-12 rounded-lg">
+        <Inbox className="h-12 w-12 text-cyan-100"/>
+        <p className="text-gray-100">Nenhum post encontrado</p>
+      </div>
+      </div>
+    )}
     </div>
   )
 }
